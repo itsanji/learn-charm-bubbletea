a43b25180eb42ebec533c365901740eb90c27959
anjiboss <nhu.nhu.face@gmail.com>
2022-11-06 20:22:20 +0900

    shopping list done - quite simple

--- main.go
+++ main.go
@@ -7,39 +7,77 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type Model struct {
-	count int
+type ShopList struct {
+	choices  []string         // item on the list
+	cursor   int              // which item out cursor is pointing at
+	selected map[int]struct{} // selected item
 }
 
-func (m *Model) Init() tea.Cmd {
+func initialModel() ShopList {
+	return ShopList{
+		choices:  []string{"Carrot", "Tofu", "Blueberries", "Orange"},
+		cursor:   0,
+		selected: make(map[int]struct{}),
+	}
+}
 
+func (m ShopList) Init() tea.Cmd {
 	return nil
 }
 
-func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m ShopList) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c":
+		case "ctrl+c", "q":
 			return m, tea.Quit
-		case "up":
-			m.count++
-		case "down":
-			m.count--
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices) {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
 		}
 	}
 	return m, nil
 }
 
-func (m *Model) View() string {
+func (m ShopList) View() string {
+	s := "What should we buy in the market? \n"
+	for i, choice := range m.choices {
+		// Is curosr pointing at this record ?
+		pointer := " "
+		if m.cursor == i {
+			pointer = ">"
+		}
 
-	return fmt.Sprintf("Count: %d \n\n   Increase ↑      Decrease ↓", m.count)
+		// Is this record selected
+		isSelected := " "
+		if _, ok := m.selected[i]; ok {
+			isSelected = "X"
+		}
+
+		// Render the row
+		row := fmt.Sprintf("%s [%s] %s \n", pointer, isSelected, choice)
+		s += row
+	}
+	s += "\n\n\nPress Q to quit."
+	return s
 }
 
 func main() {
-	err := tea.NewProgram(&Model{}, tea.WithAltScreen()).Start()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	p := tea.NewProgram(initialModel())
+	if err := p.Start(); err != nil {
+		fmt.Printf("There's an error %v", err)
 		os.Exit(1)
 	}
 }
